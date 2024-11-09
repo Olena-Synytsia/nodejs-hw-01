5b9fac1f8ee559fb907eb3c746c9475660ee44f4
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-11-10 00:05:18 +0200

    step 1-4

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array.from({ length: number }, createFakeContact);
+  const updatedContacts = [...contacts, ...newContacts];
+  await writeContacts(updatedContacts);
+};
 
 generateContacts(5);
